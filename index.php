4de70166eb031f206ad1d13fc7c74759ff6b81be
luaanHD <luantoniolo94@gmail.com>
2023-07-31 17:51:32 +0000

    Logo já feita e escalada

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>##</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
 </head>
+
 <body>
-    
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-10">
+                <img src="/img/logo/logo_1nova.jpeg" width="150px" height="165px" alt="logo do nosso site">
+            </div>
+            <div class="col-1">
+                <button type="button"><a href="login">Entrar</a></button>
+            </div>
+            <div class="col-1">
+                <button type="button"><a href="cadastrar">Cadastre-se</a></button>
+            </div>
+        </div>
+        
+    </div>
+
 </body>
+
 </html>
