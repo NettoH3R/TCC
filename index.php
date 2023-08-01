c6a0d9c1e89aba24a02533b296c69ba88e4daa9b
LipzinXD <felipescamargo806@gmail.com>
2023-08-01 01:14:44 +0000

    index

--- index.php
+++ index.php
@@ -7,21 +7,53 @@
     <title>##</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css">
 </head>
+<style>
+    .centro {
+        list-style: none;
+        display: flex;
+        justify-content: space-around;
+    }
+
+   .direita{
+    display: flex;
+    justify-content: flex-end;
+    height: 50px;
+    flex-direction: c;
+   }
+   
+    .menu {
+        background-color: orangered;
+    }
+</style>
 
 <body>
     <div class="container">
-        <div class="row text-center">
-            <div class="col-10">
+        <div class="row">
+            <div class="centro">
                 <img src="/img/logo/logo_1nova.jpeg" width="150px" height="165px" alt="logo do nosso site">
             </div>
-            <div class="col-1">
-                <button type="button"><a href="login">Entrar</a></button>
+            <div class="direita">
+                <button class="btn btn-primary me-md-2" type="button">Login</button>
             </div>
-            <div class="col-1">
-                <button type="button"><a href="cadastrar">Cadastre-se</a></button>
+            <div class="direita">
+                <button class="btn btn-primary me-md-2" type="button">Cadastre-se</button>
             </div>
         </div>
-        
+        <div>
+            <ul class="centro menu">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Generos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre</a>
+                </li>
+            </ul>
+        </div>
+
+
     </div>
 
 </body>
