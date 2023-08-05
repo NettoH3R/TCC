03d1dadfb9ca30607082b39977abec412accb540
João Netto <jovinettonetto@gmail.com>
2023-08-05 20:56:03 +0000

    modify

--- index.php
+++ index.php
@@ -12,70 +12,75 @@
 
     <style>
         .tamanhologo {
-            width: 100%;
+            max-width: 100%;
+            height: 100%;
         }
 
-        main#menu {
+        nav#menu {
             background-color: #244bbf;
         }
     </style>
 
 </head>
 
-<body id="">
-    <header id="cabecario">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-sm-5 text-center">
-                </div>
-                <div class="col-md-2 col-sm-2 col-xs-8 text-center">
-                    <img src="./img/logo/logo_1nova.png" class="tamanhologo" alt="Logo do nosso site">
-                </div>
-                <div class="col-md-2 col-sm-0">
+<body>
+    <main id="Tudo">
+        <header id="cabecario">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 text-center">
+                    </div>
+                    <div class="col-md-2 text-center">
+                        <img src="./img/logo/logo_1nova.png" class="tamanhologo" alt="Logo do nosso site">
+                    </div>
+                    <div class="col-md-2">
 
-                </div>
-                <div class="col-md-3 col-sm-5 col-xs-4 text-center">
-                    <button type="button" class="btn-1">Entrar</button>
-                    <button type="button" class="btn-2">Cadastrar</button>
+                    </div>
+                    <div class="col-md-3 text-center">
+                        <button type="button" class="btn-1">Entrar</button>
+                        <button type="button" class="btn-2">Cadastrar</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-    <nav id="menu" class="container">
-        <div class="row">
-            <div class="col-md-12">
+        </header>
+        <nav id="menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
 
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li class="naoclicavel"><a href="#">|</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Gêneros
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li class="naoclicavel"><a href="#">|</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Gêneros
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
                             </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li class="naoclicavel"><a href="#">|</a></li>
+                            <li><a href="sobre.php">Sobre</a></li>
                         </ul>
-                    </li>
-                    <li class="naoclicavel"><a href="#">|</a></li>
-                    <li><a href="sobre.php">Sobre</a></li>
-                </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
 
-    <main>
-        <h2>Main</h2>
+        <main>
+            <h2>Main</h2>
+        </main>
+
+        <footer>
+            <h4>@copy desenvolvido pela Equipe 1 | 3ºETIM 2023</h4>
+        </footer>
     </main>
-
-    <footer>
-        <h4>@copy desenvolvido pela Equipe 1 | 3ºETIM 2023</h4>
-    </footer>
 </body>
 
 </html>
