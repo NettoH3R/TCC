664cc6b3b9cf58c69d7b60cc8d6825b65e59fabb
João Netto <jovinettonetto@gmail.com>
2023-08-06 18:37:40 +0000

    retrocesso

--- index.php
+++ index.php
@@ -6,12 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>##</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/feather-icons"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/defalut.css">
 
     <style>
+        .tamanhologo {
+            max-width: 100%;
+            height: 100%;
+        }
+
         nav#menu {
             background-color: #244bbf;
         }
@@ -21,58 +25,58 @@
 
 <body>
     <main id="Tudo">
-
-
-        <!-- Cabeçario -->
-
-
         <header id="cabecario">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-5 col-sm-7 ">
+                    <div class="col-md-5 text-center">
                     </div>
                     <div class="col-md-2 text-center">
                         <img src="./img/logo/logo_1nova.png" class="tamanhologo" alt="Logo do nosso site">
                     </div>
-                    <div class="col-md-2 ">
+                    <div class="col-md-2">
 
                     </div>
-                    <div class="col-md-3  col-sm-5 text-center">
+                    <div class="col-md-3 text-center">
                         <button type="button" class="btn-1">Entrar</button>
                         <button type="button" class="btn-2">Cadastrar</button>
                     </div>
                 </div>
             </div>
         </header>
+        <div class="container">
+            <nav id="menu">
 
+                <div class="row">
+                    <div class="col-md-12">
 
-        <!-- Menu -->
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li class="naoclicavel"><a href="#">|</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Gêneros
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="naoclicavel"><a href="#">|</a></li>
+                            <li><a href="sobre.php">Sobre</a></li>
+                        </ul>
+                    </div>
+                </div>
 
+            </nav>
+        </div>
 
-        <!-- <div class="container"> -->
-        <nav id="menu" class="text-center">
-                <ul id="menu" class="menu">
-
-                    <a href="index.php">
-                        <li class="item active">Home</li>
-                    </a>
-
-
-                    <a href="index.php">
-                        <li class="item">Generos</li>
-                    </a>
-
-
-                    <a href="sobre.php">
-                        <li class="item">Sobre</li>
-                    </a>
-
-                </ul>
-        </nav>
-        <!-- </div> -->
 
         <main>
-            <!-- <div class="container">
+            <div class="container">
                 <div id="carouselExampleCaptions" class="carousel slide">
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -81,14 +85,14 @@
                     </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-bs-interval="1000">
-                            <img src="img/logo/download.jpg" class="d-block w-100" alt="...">
+                            <img src="img/logo/download.jpg" class="d-block w-100" alt="..." >
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>First slide label</h5>
                                 <p>Some representative placeholder content for the first slide.</p>
                             </div>
                         </div>
                         <div class="carousel-item" data-bs-interval="1000">
-                            <img src="img/logo/download.jpg" class="d-block w-100" alt="...">
+                            <img src="img/logo/download.jpg" class="d-block w-100" alt="..." >
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Second slide label</h5>
                                 <p>Some representative placeholder content for the second slide.</p>
@@ -111,7 +115,7 @@
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
-            </div> -->
+            </div>
         </main>
 
         <footer>
